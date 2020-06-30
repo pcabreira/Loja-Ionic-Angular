f397fab0c352f17704b78a0179219ff21c126520
Pedro Cabreira <pedro.cabreira.vendor@europ-assistance.com.br>
2020-06-30 15:32:08 -0300

    Correção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,9 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDqETbdki_q8xGNU-v3Zax818jtUjxPvio",
-    authDomain: "sabordafeira-336dc.firebaseapp.com",
-    databaseURL: "https://sabordafeira-336dc.firebaseio.com",
-    projectId: "sabordafeira-336dc",
-    storageBucket: "sabordafeira-336dc.appspot.com",
-    messagingSenderId: "181664974211",
-    appId: "1:181664974211:web:f90fbf35e8f1564edee2b6",
-    measurementId: "G-P312915KRZ"
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    storageBucket: '<your-storage-bucket-url>'
   }
  };
